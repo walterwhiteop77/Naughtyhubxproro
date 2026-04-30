2c0b30f39fbe0f247ad1b100b219bfa450a6aa21
walterwhiteop77 <walterwhite2k27@gmail.com>
2026-04-30 14:57:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,17 +30,17 @@ DB_NAME = environ.get("DATABASE_NAME", "Cluster0")
 # =========================================================
 ADMINS = int(environ.get("ADMINS", "6725874739"))
 
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002031127227"))
-PREMIUM_LOGS = int(environ.get("PREMIUM_LOGS", "-1002702898412"))
-VERIFIED_LOG = int(environ.get("VERIFIED_LOG", "-1002378958723"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1003719289913"))
+PREMIUM_LOGS = int(environ.get("PREMIUM_LOGS", "-1003798512506"))
+VERIFIED_LOG = int(environ.get("VERIFIED_LOG", "-1003923118233"))
 
-POST_CHANNEL = int(environ.get("POST_CHANNEL", "-1002843484373"))
-VIDEO_CHANNEL = int(environ.get("VIDEO_CHANNEL", "-1002756243661"))
-BRAZZER_CHANNEL = int(environ.get("BRAZZER_CHANNEL", "-1002737831383"))
-CATEGORY_CHANNELS = "Desi:-1003502198200 Videsi:-1003972520497 Leaked:-1003939725409 Snaps:-1003989423889"
+POST_CHANNEL = int(environ.get("POST_CHANNEL", "-1003871870272"))
+VIDEO_CHANNEL = int(environ.get("VIDEO_CHANNEL", "-1003669040304"))
+BRAZZER_CHANNEL = int(environ.get("BRAZZER_CHANNEL", "-1003782456217"))
+CATEGORY_CHANNELS = int(environ.get("Desi:-1003502198200 Videsi:-1003972520497 Leaked:-1003939725409 Snaps:-1003989423889")
 
 # Auth channels list
-auth_channel_str = environ.get("AUTH_CHANNEL", "-1002843484373")
+auth_channel_str = environ.get("AUTH_CHANNEL", "-1001610198839")
 AUTH_CHANNEL = [int(x) for x in auth_channel_str.split() if x.strip().lstrip("-").isdigit()]
 
 # =========================================================
@@ -55,7 +55,7 @@ PROTECT_CONTENT = str_to_bool(environ.get("PROTECT_CONTENT"), True)
 # =========================================================
 # 🔢 LIMITS
 # =========================================================
-DAILY_LIMIT = int(environ.get("DAILY_LIMIT", "5"))
+DAILY_LIMIT = int(environ.get("DAILY_LIMIT", "15"))
 VERIFICATION_DAILY_LIMIT = int(environ.get("VERIFICATION_DAILY_LIMIT", "700"))
 PREMIUM_DAILY_LIMIT = int(environ.get("PREMIUM_DAILY_LIMIT", "50050"))
 BOOKMARK_LIMIT_FREE = int(environ.get("BOOKMARK_LIMIT_FREE", "5"))
@@ -68,21 +68,21 @@ SHORTLINK_API = environ.get("SHORTLINK_API", "f9da968c27a8594f2bbc3b2cd1e8778fa7
 POST_SHORTLINK_URL = environ.get("POST_SHORTLINK_URL", "vplink.in")
 POST_SHORTLINK_API = environ.get("POST_SHORTLINK_API", "f9da968c27a8594f2bbc3b2cd1e8778fa756b3a5")
 VERIFY_EXPIRE = int(environ.get("VERIFY_EXPIRE", "3600"))
-TUTORIAL_LINK = environ.get("TUTORIAL_LINK", "https://t.me/+ny_72fisEuJmMmI1")
+TUTORIAL_LINK = environ.get("TUTORIAL_LINK", "https://t.me/tutorial_filx/7")
 
 # =========================================================
 # 💳 PAYMENT SETTINGS
 # =========================================================
-UPI_ID = environ.get("UPI_ID", "fff")
-QR_CODE_IMAGE = environ.get("QR_CODE_IMAGE", "https://i.ibb.co/Q3fzgQ69/x.jpg")
+UPI_ID = environ.get("UPI_ID", "flixhd@ptaxis")
+QR_CODE_IMAGE = environ.get("QR_CODE_IMAGE", "https://i.ibb.co/Rk2fXKN3/x.jpg")
 
 # =========================================================
 # 🖼️ IMAGES
 # =========================================================
-START_PIC = environ.get("START_PIC", "https://i.ibb.co/Q3fzgQ69/x.jpg")
-AUTH_PICS = environ.get("AUTH_PICS", "https://i.ibb.co/Q3fzgQ69/x.jpg")
-VERIFY_IMG = environ.get("VERIFY_IMG", "https://i.ibb.co/Q3fzgQ69/x.jpg")
-NO_IMG = environ.get("NO_IMG", "https://i.ibb.co/Q3fzgQ69/x.jpg")
+START_PIC = environ.get("START_PIC", "https://i.ibb.co/nMfrCRSw/x.jpg")
+AUTH_PICS = environ.get("AUTH_PICS", "https://i.ibb.co/rfNnbhV9/x.jpg")
+VERIFY_IMG = environ.get("VERIFY_IMG", "https://i.ibb.co/1JbhhN74/x.jpg")
+NO_IMG = environ.get("NO_IMG", "https://i.ibb.co/5xc7m3cd/x.jpg")
 
 # =========================================================
 # 🌐 WEB APP
